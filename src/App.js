b7539903c063c036e68ebc307c268a890a97db94
Radha Satam <radha.satam@gmail.com>
2019-08-23 13:22:48 +0530

    refactoredr

--- src/App.js
+++ src/App.js
@@ -1,84 +1,59 @@
 import React, {Component} from "react";
+import { numbers, actions } from './data/calcData';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state= {
-      history: [],
-      inputvalue: "",
+      // history: [],
+      input: "",
+      currentAction: ''
+    }
+    this.evaluate = this.evaluate.bind(this)
+  }
+
+  numberHandler(number) {
+    this.setState({ input: this.state.input + number })
+  }
+
+  actionHandler(action) {
+    let input = this.state.input,
+        lastChar = input ? input.charAt(input.length - 1) : null;
+    // input should exist and previously entered value should not be an action
+    if(input && !actions.includes(lastChar)) {
+      this.setState({ currentAction: action, 
+                      input: `${input}${action}`
+                    })
     }
   }
-  buttonPress1 =(e)=>{
-    console.log(e.target.value);
-    this.setState({
-     inputvalue: this.state.inputvalue + e.target.value
-    })
-  }
-
-
-  buttonPressAns =()=>{
-    var newArr = [...this.state.history,{value1 : this.state.inputvalue, value2: eval(this.state.inputvalue)}]
-    console.log(newArr);
-    this.setState({
-      inputvalue: eval(this.state.inputvalue),
-      history: newArr
-    })
-  }
   
-  buttonPressClear =()=>{
-    this.setState({
-      inputvalue: ""
-    })
+  evaluate() {
+    try {
+      // console.log('this.state.input', this.state.input)
+      this.setState({ input: eval(this.state.input).toString() })
+    }
+    catch (err) {
+      // console.log('there was an err', err)
+    }
   }
-  buttonPressC =()=>{
-    this.setState({
-      inputvalue: this.state.inputvalue.slice(0,this.state.inputvalue.length-1)
-    })
-  }
-  buttonPressHist =()=>{
-    this.setState({
-      inputvalue: this.state.history.map( value => { return value.value1+"="+ value.value2})
-    })
-  }
+
   render(){
-  return (
-    <div className="App">
-      <div className = "input">
-         <input type="text" value={this.state.inputvalue}></input> 
+    console.log('this.state', this.state);
+
+    return (
+      <div className="App"> 
+        <input type="text" value={this.state.input} readOnly></input> 
+        {
+          numbers.map(val => <button key={val} onClick={() => this.numberHandler(val)}>{val}</button>)
+        }
+        {
+          actions.map(action => <button key={action} onClick={() => this.actionHandler(action)}>{action}</button>)
+        }
+        <button onClick={this.evaluate}>=</button>
       </div>
-      <div className= "buttons">
-        <div className = "buttonGrp1">
-        <button type="button" value = "1" onClick= {this.buttonPress1}>1</button>
-        <button type="button" value = "2" onClick= {this.buttonPress1}>2</button>
-        <button type="button" value = "3" onClick= {this.buttonPress1}>3</button>
-        <button type="button"  onClick= {this.buttonPressAns}>=</button>
-        </div>
-        <div className = "buttonGrp2">
-        <button type="button" value = "4" onClick= {this.buttonPress1}>4</button>
-        <button type="button" value = "5" onClick= {this.buttonPress1}>5</button>
-        <button type="button" value = "6" onClick= {this.buttonPress1}>6</button>
-        <button type="button" value = "+" onClick= {this.buttonPress1}>+</button>
-        </div>
-        <div className = "buttonGrp3">
-        <button type="button" value = "7" onClick= {this.buttonPress1}>7</button>
-        <button type="button" value = "8" onClick= {this.buttonPress1}>8</button>
-        <button type="button" value = "9" onClick= {this.buttonPress1}>9</button>
-        <button type="button" value = "-" onClick= {this.buttonPress1}>-</button>
-        </div>
-        <div className = "buttonGrp4">
-        <button type="button" value = "*" onClick= {this.buttonPress1}>*</button>
-        <button type="button" value = "0" onClick= {this.buttonPress1}>0</button>
-        <button type="button" value = "/" onClick= {this.buttonPress1}>/</button>
-        <button type="button"  onClick= {this.buttonPressC}>C</button>
-        </div>
-        <div className = "buttonGrp5">
-        <button type="button"  onClick= {this.buttonPressClear}>Clear</button>
-        <button type="button"  onClick= {this.buttonPressHist}>Hist</button>
-        </div>
-        
-      </div>
-    </div>
-  );
+    );
+  }
 }
-}export default App;
+
+export default App;
---
+++ src/data/calcData.js
@@ -0,0 +1,2 @@
+export const numbers = Array(9).fill(0).map((e, i) => i+1);
+export const actions = ['/', '*', '-', '+']
